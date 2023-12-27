d21411bc606cec3272731624c37b1d5ab3533508
Sina Zinsaz <sinazinsaz1@gamil.com>
2023-12-27 13:04:50 +0330

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Calendar from "./Component/calendar/Calendar";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 function App() {
-  return <Calendar />;
+  return (
+    <DarkModeProvider>
+      <Calendar />
+    </DarkModeProvider>
+  );
 }
 
 export default App;
--- src/Component/calendar/Calendar.jsx
+++ src/Component/calendar/Calendar.jsx
@@ -9,39 +9,49 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { useState } from "react";
+
+// dsasda
+import { useDarkMode } from "../../context/DarkModeContext";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 export default function Calendar() {
-  const [dark, setDark] = useState("false");
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
     <Box>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Box display="flex" alignItems="center" pt="0.5rem">
-            <ArrowBackIosNewIcon fontSize="5px" />
-            <Typography variant="body1" color="initial" px="3rem">
-              بهمن
-            </Typography>
-            <ArrowForwardIosIcon fontSize="5px" />
-          </Box>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Box display="flex" alignItems="center" pt="0.5rem">
+              <ArrowBackIosNewIcon fontSize="5px" />
+              <Typography variant="body1" px="3rem">
+                بهمن
+              </Typography>
+              <ArrowForwardIosIcon fontSize="5px" />
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box display="flex" justifyContent="end" alignItems="center">
+              <IconButton color="inherit" onClick={toggleDarkMode}>
+                {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
+              <Typography variant="body1" pr="4px" fontWeight="bold">
+                برنامه هفتگی
+              </Typography>
+              <CalendarMonthIcon />
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Box display="flex" justifyContent="end" alignItems="center">
-            <IconButton color="inherit" onClick={() => setDark(!dark)}>
-              {dark ? <Brightness4Icon /> : <Brightness7Icon />}
-            </IconButton>
-            <Typography
-              variant="body1"
-              color="initial"
-              pr="4px"
-              fontWeight="bold"
-            >
-              برنامه هفتگی
-            </Typography>
-            <CalendarMonthIcon />
-          </Box>
-        </Grid>
-      </Grid>
+      </ThemeProvider>
     </Box>
   );
 }
