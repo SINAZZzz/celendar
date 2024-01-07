a195d7f90f66256e15b32494a7bbed4926c26f6a
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-01-07 19:36:49 +0330

    add new update

--- src/Component/calendar/Calendar.jsx
+++ src/Component/calendar/Calendar.jsx
@@ -1,23 +1,8 @@
 import Header from "./component/Header";
 import { days, number } from "../../Data/data";
 import { Typography, Box, Grid, Container } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles((theme) => ({
-  customCard: {
-    backgroundColor: theme.palette.primary.background,
-    border: theme.palette.primary.border,
-  },
-  customContentCard: {
-    fontWeight: theme.palette.primary.bold,
-    background: theme.palette.primary.backp,
-    fontFamily: "YekanBakh",
-  },
-}));
 
 export default function Calendar() {
-  const classes = useStyles();
-
   return (
     <Container>
       <Box>
@@ -30,7 +15,6 @@ export default function Calendar() {
                 alignItems: "center",
                 justifyContent: "center",
                 display: "flex",
-                // mx: "1px",
                 py: "1rem",
               }}
               item
@@ -45,7 +29,6 @@ export default function Calendar() {
           {number.map((d, index) => (
             <Grid key={index} item xs={12} md={1.6} sm={1.6} lg={1.714285}>
               <Box
-                className={classes.customCard}
                 component="div"
                 sx={{
                   alignItems: "center",
@@ -54,11 +37,12 @@ export default function Calendar() {
                   m: "3px",
                   py: "10%",
                   borderRadius: "5px",
+                  background: (theme) => theme.palette.primary.background,
+                  border: (theme) => theme.palette.primary.border,
                 }}
               >
                 <Typography variant="body1">
                   <Box
-                    className={classes.customContentCard}
                     component="p"
                     sx={{
                       display: "flex",
@@ -68,6 +52,9 @@ export default function Calendar() {
                       fontSize: "30px",
                       width: "50px",
                       height: "50px",
+                      fontWeight: (theme) => theme.palette.primary.bold,
+                      background: (theme) => theme.palette.primary.backp,
+                      fontFamily: "YekanBakh",
                     }}
                   >
                     {d}
@@ -77,86 +64,6 @@ export default function Calendar() {
             </Grid>
           ))}
         </Grid>
-        {/* <Box display="flex" justifyContent="center">
-        {days.map((item, index) => (
-          <Typography key={index} px="4%">
-            {item}
-          </Typography>
-        ))}
-      </Box> */}
-
-        {/* <Grid direction="row-reverse" container>
-        {number.map((d, index) => (
-          <Grid
-            key={index}
-            className={classes.customCard}
-            sx={{
-              alignItems: "center",
-              justifyContent: "center",
-              display: "flex",
-              mx: "1px",
-              py: "1rem",
-              borderRadius: "5px",
-            }}
-            item
-            xs={12}
-            md={1.6}
-            sm={1.6}
-            lg={1.714285}
-          >
-            <Typography variant="body1">
-              <Box
-                className={classes.customContentCard}
-                component="p"
-                sx={{
-                  background: "rgba(220,220,220 , 0.40)",
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  width: "30px",
-                  height: "30px",
-                }}
-              >
-                {d}
-              </Box>
-            </Typography>
-          </Grid>
-        ))}
-      </Grid> */}
-        {/* <Box>
-        <Box
-          className={classes.customCard}
-          sx={{
-            alignItems: "center",
-            justifyContent: "center",
-            px: "2rem",
-            py: "2rem",
-            borderRadius: "5px",
-            width: "fit-content",
-          }}
-        >
-          <Typography variant="body1">
-            <Box
-              className={classes.customContentCard}
-              // component="p"
-              sx={{
-                background: "rgba(220,220,220 , 0.40)",
-                borderRadius: "50%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                width: "30px",
-                height: "30px",
-                // px: "15px",
-                // py: "10px",
-              }}
-            >
-              1
-            </Box>
-          </Typography>
-        </Box>
-      </Box> */}
       </Box>
     </Container>
   );
